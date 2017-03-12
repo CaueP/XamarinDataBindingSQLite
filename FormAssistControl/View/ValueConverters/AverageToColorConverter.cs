b454c2647e8877cd7c5cb97858fa0e339703b4b3
Caue Polimanti <caue.polimanti@gmail.com>
2017-03-12 19:01:05 -0300

    Implementing a Value Converter - Created AverageToColorConverter that changes the color of the name according to the average

---
+++ FormAssistControl/View/ValueConverters/AverageToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace FormAssistControl
+{
+	public class AverageToColorConverter:IValueConverter
+	{
+		public AverageToColorConverter()
+		{
+		}
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			// converting entry value to double
+			double average = (double)value;
+
+			return average < 60 ? Color.Red : Color.Blue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
